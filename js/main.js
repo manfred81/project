4ce896bd12773cb61d0def7ace460ae8af47667a
manfred81 <igor_travkin@mail.ru>
2022-02-13 17:45:49 +0300

    dz1

--- js/main.js
+++ js/main.js
@@ -7,19 +7,19 @@ const products = [
   {id: 4, title: 'Gamepad', price: 150},
 ];
 
-const renderProduct = (title, price) => {
-  return `<div class="product-item">
+const renderProduct = ((title, price) => 
+   `<div class="product-item">
             <h3>${title}</h3>
             <p>${price}</p>
             <button class="by-btn">Добавить</button>
-          </div>`;
-};
+          </div>`
+);
 
 const renderProducts = list => {
-  const productList = list.map(item => renderProduct(item.title, item.price));
-  console.log(productList);
-  document.querySelector('.products').innerHTML = productList.join('');
-};
+  const productList = (list.map(product => 
+    renderProduct(product.title, product.foto, product.price))).join('');
+  document.querySelector('.products').innerHTML = productList;
+  };
 
 // второй вариант
 
